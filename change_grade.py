7dd691edada60ab5a9a4c14487ce9b3924d4f57f
Likhitha Patha <likhitha@vt.edu>
2012-03-21 13:08:58 -0400

    Added a "__main__" block that calls the new function and implements the same input/output behavior (in terms of command line arguments, standard in and standard out/error) as the original script.

--- change_grade.py
+++ change_grade.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
 import re 
-import fileinput
-from sys import stderr,stdin,argv,exit
+#import fileinput
+#from sys import stderr,stdin,argv,exit
 
+def change_grade(usr_in):
+	"""Takes the student name and grade and updates the information in the file accordingly"""
+	changes = ""
+	for line in usr_in:
+		if argv[1] in line: 
+			changes += re.sub('[0-9].*',argv[2],line)
+		else:
+			changes += line
+	return changes
 
-if len(argv) !=3:
-    stderr.write("Usage: "+ argv[0] + " STRING NUMBER \n")
-    exit(1)
-
-result = []
-for line in stdin:
-	if argv[1] in line: 
-		print re.sub('[0-9].*',argv[2],line),
-	else:
-		print line
-exit(0)
-		
+if __name__=='__main__':
+    from sys import stdin,stdout,stderr,argv
+    if len(argv) !=3:
+        stderr.write("Usage: "+ argv[0] + " STRING NUMBER \n")
+        exit(1)
+    (result) = change_grade(stdin)
+    #print result    
+    stdout.write(result)
